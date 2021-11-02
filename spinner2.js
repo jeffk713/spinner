7c6a2fa26f7509d102404ae89f13d950dd285e59
jeff <jeffk713@gmail.com>
2021-11-02 13:38:27 -0700

    finish

---
+++ spinner2.js
@@ -0,0 +1,12 @@
+const spinnerArr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+let counter = 0;
+
+for (let i = 0; i < 5000; i += 200) {
+  const index = counter % 4;
+
+  setTimeout(() => {
+    process.stdout.write(spinnerArr[index]);
+  }, i);
+
+  counter++;
+}
